3f95b8c02d043b098b63554ee2cb95b9b46fdc6f
trebolsoft <trebolsoftv1@gmail.com>
2025-12-05 10:22:18 -0500

    Sincronizando cambios locales

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
 set -e
-/app/wait-for-it.sh db:5432 --timeout=120 --strict -- echo "DB is up"
-alembic upgrade head
-exec uvicorn app.main:app --host 0.0.0.0 --port 10000
+uvicorn app.main:app --host 0.0.0.0 --port 10000
