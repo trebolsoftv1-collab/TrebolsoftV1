847be20bec4ee9225de40483b320efb1f2144d2d
trebolsoft <trebolsoftv1@gmail.com>
2025-12-05 14:13:54 -0500

    Sincronizando cambios locales

--- restore_temp/application/app/core/dependencies.py
+++ restore_temp/application/app/core/dependencies.py
@@ -65,7 +65,7 @@ def get_current_active_supervisor(
     """
     Verifica que el usuario actual sea supervisor o admin.
     """
-    if current_user.role not in ["admin", "supervisor"]:
+    if current_user.role not in ["ADMIN", "SUPERVISOR"]:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Not enough permissions"
